42b771cc21150aef86926e2b118f1d8b101ec9dd
Nawaat1 <bwb-n.monsofi@fondespierre.com>
2022-12-07 18:27:39 +0100

    media query

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll",()=>{
     header.classList.toggle ("sticky", window.scrollY > 0)
 })
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+// Au click sur l'icon menu ouvrir la navbar
+menu.addEventListener("click", ()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+})
